e62fb95ab41c63b2b14b3fc56f00a649077da145
game123ahmed@gmail.com <game123ahmed@gmail.com>
2023-03-10 14:18:24 +0600

    new code added

--- string/very_easy/compare_strings_by_count_of_characters_07/src/App.js
+++ string/very_easy/compare_strings_by_count_of_characters_07/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+function strLength(str1,str2){
+
+  let str = ""
+
+  if(str1.length === str2.length){
+    str += true
+
+  }
+  else{
+    str += false
+  }
+
+  return str
 }
 
-export default App;
+
+const App=()=>{
+
+  const [str1,setStr1] = useState("")
+  const [str2,setStr2] = useState("")
+  const [res,setRes] = useState("")
+
+
+  const handleClick=()=>{
+    let result = strLength(str1,str2)
+    setRes(result)
+  }
+
+
+
+
+
+  return(
+    <div>
+
+      <h1>Compare Strings by Count of Characters</h1>
+      <input type="text" value={str1} onChange={(e)=>setStr1(e.target.value)}/>
+      <input type="text" value={str2} onChange={(e)=>setStr2(e.target.value)}/>
+      <button onClick={handleClick}>Click</button>
+      <h3>The Final output is:{res}</h3>
+    </div>
+  )
+}
+
+
+export default App
